2ed3d71316dc0cd5703afbe17eababe1624610ea
lianfu <lianfu@syn-069-202-180-025.res.spectrum.com>
2024-12-15 23:12:38 -0500

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+import './scss/index.scss';
+
+const App = () => {
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome</h1>
+    </>
+  );
+};
+
+export default App;
